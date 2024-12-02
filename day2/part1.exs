a66536042c63f176ab943f87ba33376b8bbffdba
Francesco Bedussi <francesco@francescobedussi.it>
2024-12-02 18:14:16 +0100

    day2

--- day2/part1.exs
+++ day2/part1.exs
@@ -1,19 +1,43 @@
-test = false
+defmodule Helpers do
+  def isSafe(report) do
+    isSorted(report) && diffIsOk(report)
+  end
+
+  defp isSorted(report) do
+    sorted = Enum.sort(report)
+    report === sorted || Enum.reverse(report) === sorted
+  end
+
+  defp checkDiff(item, next) do
+    diff = abs(item - next)
+    diff >= 1 && diff <= 3
+  end
+
+  defp diffIsOk([item | [next | []]]) do
+    checkDiff(item, next)
+  end
+
+  defp diffIsOk([item | [next | rest]]) do
+    if (checkDiff(item, next)) do
+      diffIsOk([next | rest])
+    else
+      false
+    end
+  end
+end
+
+test =  false
 filePath = if test, do: "test-data.txt", else: "data.txt"
 
 {:ok, rawData} = File.read(filePath)
 
 rawData
   |> String.split("\n", trim: true)
-  |> Enum.map(fn line -> line |> String.split("   ", trim: true) |> Enum.map(&String.to_integer/1) end)
+  |> Enum.map(fn line -> line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1) end)
   |> Enum.filter(fn line -> line != "" end)
   |> IO.inspect(label: "input")
-  |> Enum.zip()
-  |> Enum.map(&Tuple.to_list/1)
-  |> IO.inspect(label: "2 lists")
-  |> Enum.map(&Enum.sort/1)
-  |> IO.inspect(label: "2 lists sorted")
-  |> Enum.zip()
-  |> Enum.map(fn {left, right} -> abs(right - left) end)
-  |> Enum.sum()
+
+  |> Enum.filter(&Helpers.isSafe/1)
+  |> length
   |> IO.inspect(label: "result")
+# 490
--- day2/part2.exs
+++ day2/part2.exs
@@ -1,25 +1,81 @@
-defmodule Helpers do
-  def toListWithWeight [left|[right]] do
-    left |> Enum.map(fn leftItem -> {leftItem, Enum.count(right, fn rightItem -> rightItem == leftItem end)} end)
+defmodule InputHelpers do
+  def parse(isTest) do
+    filePath = if isTest, do: "test-data.txt", else: "data.txt"
+
+    {:ok, rawData} = File.read(filePath)
+
+    rawData
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.filter(fn line -> line != "" end)
+
+    # |> IO.inspect(label: "input")
   end
 end
 
-test = false
-filePath = if test, do: "test-data.txt", else: "data.txt"
+defmodule Helpers do
+  def explode(report) do
+    [
+      report
+      | report
+        |> Enum.with_index()
+        |> Enum.map(fn {_, index} ->
+          report |> List.delete_at(index)
+        end)
+    ]
+  end
 
-{:ok, rawData} = File.read(filePath)
+  def isSafe(report) do
+    isSorted(report) && diffIsOk(report)
+  end
 
-rawData
-  |> String.split("\n", trim: true)
-  |> Enum.map(fn line -> line |> String.split("   ", trim: true) |> Enum.map(&String.to_integer/1) end)
-  |> Enum.filter(fn line -> line != "" end)
-  |> IO.inspect(label: "input")
-  |> Enum.zip()
-  |> Enum.map(&Tuple.to_list/1)
-  |> IO.inspect(label: "2 lists")
-  |> Helpers.toListWithWeight()
-  |> IO.inspect(label: "left with weight")
-  |> Enum.map(fn {a, b} -> a * b end)
-  |> IO.inspect(label: "similarity scores")
-  |> Enum.sum
-  |> IO.inspect(label: "result")
+  defp isSorted(report) do
+    sorted = Enum.sort(report)
+    report === sorted || Enum.reverse(report) === sorted
+  end
+
+  defp checkDiff(item, next) do
+    diff = abs(item - next)
+    diff >= 1 && diff <= 3
+  end
+
+  defp diffIsOk([item | [next | []]]) do
+    checkDiff(item, next)
+  end
+
+  defp diffIsOk([item | [next | rest]]) do
+    if checkDiff(item, next) do
+      diffIsOk([next | rest])
+    else
+      false
+    end
+  end
+end
+
+defmodule Main do
+  def run(isTest) do
+    InputHelpers.parse(isTest)
+    |> Enum.map(&Helpers.explode/1)
+    |> Enum.filter(fn permutations -> permutations |> Enum.any?(&Helpers.isSafe/1) end)
+    |> length
+    |> IO.inspect(label: if(isTest, do: "test result: ", else: "real result: "))
+
+    # 536
+  end
+end
+
+defmodule Test do
+  use ExUnit.Case
+
+  ExUnit.start()
+
+  test "Day 2 - Part 2 - test data" do
+    assert Main.run(true) == 4
+  end
+
+  test "Day 2 - Part 2 - real data" do
+    assert Main.run(false) == 536
+  end
+end
